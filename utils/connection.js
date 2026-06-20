690ad9aa218ed9951283c107c5680ec0514134af
YafiAllamJunaedi <yafiallamjun@email.com>
2026-06-20 16:32:47 +0700

    hasil Projeck

--- utils/connection.js
+++ utils/connection.js
@@ -1,8 +1,6 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config"
 
-const db = new Sequelize(process.env.DB_NAME, "root", "", {
-    dialect: "mysql"
-});
+const db = new Sequelize(process.env.MYSQL_URL);
 
 export default db
